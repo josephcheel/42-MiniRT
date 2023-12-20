6c7f3b04ac2a69dbee84ebdba5e1c06ce00e0dd2
Ernesto Avedillo <eavedillo@yahoo.es>
2023-12-20 10:33:27 +0100

    pass norminette

--- inc/minirt.h
+++ inc/minirt.h
@@ -226,7 +226,7 @@ void		ft_geom_size(t_geom *head);
 void		ft_free_geometry(t_geom **head);
 void		create_field_vectors(t_field *field);
 void		def_pixel_vp(t_field *field, t_indexes in, t_indexes lim);
-t_vec_pos	get_int_pt(t_vec_pos vps, t_geom *geom, t_field *field);
+void		get_int_pt(int pixel, t_field *field);
 void		set_point_int(t_field *field);
 int			dump_mem_2_scr(t_field *field);
 t_color		set_pixel_color(t_vec_pos vp, t_vec3 vl);
