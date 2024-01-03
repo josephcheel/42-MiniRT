872a2b7218aadd93f3322a27ff4a278b43f3ef20
josephcheel <josephcheelnakashima@gmail.com>
2024-01-03 16:20:06 +0100

    norminette

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: jcheel-n <jcheel-n@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/02 12:21:33 by eavedill          #+#    #+#             */
-/*   Updated: 2024/01/02 01:59:16 by jcheel-n         ###   ########.fr       */
+/*   Updated: 2024/01/03 16:19:44 by jcheel-n         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,7 @@ int	prt_help(void)
 	return (0);
 }
 
-int	check_arg(char *s)
+int	check_extension(char *s)
 {
 	int	len_str;
 
@@ -48,61 +48,11 @@ int	check_arg(char *s)
 	return (0);
 }
 
-/*
-** Print the mandatory objects[A, L, C] message.
-*/
-int	prt_error(void)
-{
-	int	out;
-
-	out = write(2, "The file does not has All Mandatory Objects\n", 44);
-	out += write(2, "Ambient Light(A), Light(L) and Camera(C)\n", 42);
-	return (out);
-}
-
-/*
-** Check if the file has the mandatory objects[A, L, C]
-*/
-int	validate_scene_file(char *filename)
-{
-	char	**content;
-	char	*line;
-	int		fd;
-	t_vec3	elem;
-
-	elem.x = 0;
-	elem.y = 0;
-	elem.z = 0;
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-		return (write(2, "File Error\n", 12));
-	line = get_next_line(fd);
-	while (line)
-	{
-		content = ft_split(line, ' ');
-		if (ft_strcmp(content[0], "A") == 0)
-			elem.x ++;
-		else if (ft_strcmp(content[0], "L") == 0)
-			elem.y++;
-		else if (ft_strcmp(content[0], "C") == 0)
-			elem.z++;
-		ft_array_free(content, ft_array_size(content));
-		free(line);
-		line = get_next_line(fd);
-	}
-	free(line);
-	if (elem.x < 1 || elem.y < 1 || elem.z < 1)
-		return (prt_error());
-	else if (elem.x > 1 || elem.y > 1 || elem.z > 1)
-		return (write(2, "Error: Duplicated Object\n", 26));
-	return (0);
-}
-
 int	main(int ac, char **av)
 {
 	t_field	*field;
 
-	if (ac < 2 || !check_arg(av[1]))
+	if (ac < 2 || !check_extension(av[1]))
 		return (prt_help());
 	if (validate_scene_file(av[1]))
 		return (1);
