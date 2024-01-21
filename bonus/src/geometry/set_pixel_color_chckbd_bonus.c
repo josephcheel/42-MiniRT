4c993c3c1ba0b7db40ead67bbe72df09946f2aeb
josephcheel <josephcheelnakashima@gmail.com>
2024-01-21 11:58:18 +0100

    Reduce Noise

--- bonus/src/geometry/set_pixel_color_chckbd_bonus.c
+++ bonus/src/geometry/set_pixel_color_chckbd_bonus.c
@@ -6,7 +6,7 @@
 /*   By: jcheel-n <jcheel-n@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/20 23:55:29 by jcheel-n          #+#    #+#             */
-/*   Updated: 2024/01/21 03:19:52 by jcheel-n         ###   ########.fr       */
+/*   Updated: 2024/01/21 11:57:02 by jcheel-n         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,9 @@ t_color	set_pixel_color_chckdb(t_int_pts vp, t_field *field)
 	int		b;
 	int		c;
 
-	a = floor((int)(vp.pt.pt.x) / ((field->chckbd.size)));
-	b = floor((int)(vp.pt.pt.y) / ((field->chckbd.size)));
-	c = floor((int)(vp.pt.pt.z) / ((field->chckbd.size)));
+	a = ceil(round(vp.pt.pt.x) / (round(field->chckbd.size)));
+	b = ceil(round(vp.pt.pt.y) / (round(field->chckbd.size)));
+	c = ceil(round(vp.pt.pt.z) / (round(field->chckbd.size)));
 	if (((int)a + (int)b + (int)c) % 2 == 0)
 		return (field->chckbd.c1);
 	else
