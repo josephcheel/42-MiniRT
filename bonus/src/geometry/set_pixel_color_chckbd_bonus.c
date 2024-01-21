53a17f193bf9829064ce65356b9833683e0173f8
josephcheel <josephcheelnakashima@gmail.com>
2024-01-21 03:20:21 +0100

    CHECKBOARD FINISHED

--- bonus/src/geometry/set_pixel_color_chckbd_bonus.c
+++ bonus/src/geometry/set_pixel_color_chckbd_bonus.c
@@ -6,7 +6,7 @@
 /*   By: jcheel-n <jcheel-n@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/20 23:55:29 by jcheel-n          #+#    #+#             */
-/*   Updated: 2024/01/21 02:16:13 by jcheel-n         ###   ########.fr       */
+/*   Updated: 2024/01/21 03:19:52 by jcheel-n         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,13 @@
 
 t_color	set_pixel_color_chckdb(t_int_pts vp, t_field *field)
 {
-	double		a;
-	double		b;
-	double		c;
+	int		a;
+	int		b;
+	int		c;
 
-	a = round((vp.pt.pt.x)) / ((field->chckbd.size) / 2);
-	b = round((vp.pt.pt.y)) / ((field->chckbd.size) / 2);
-	c = round((vp.pt.pt.z)) / ((field->chckbd.size) / 2);
+	a = floor((int)(vp.pt.pt.x) / ((field->chckbd.size)));
+	b = floor((int)(vp.pt.pt.y) / ((field->chckbd.size)));
+	c = floor((int)(vp.pt.pt.z) / ((field->chckbd.size)));
 	if (((int)a + (int)b + (int)c) % 2 == 0)
 		return (field->chckbd.c1);
 	else
