0caa38b07b33967c36c6489cc3278bf7bcf42d04
Joseph42 <jcheel-n@student.42barcelona.com>
2024-01-22 20:11:27 +0100

    Just a test

--- bonus/src/geometry/set_pixel_color_bumpmap_bonus.c
+++ bonus/src/geometry/set_pixel_color_bumpmap_bonus.c
@@ -14,17 +14,33 @@
 
 t_color set_pixel_color_bumpmap(t_int_pts vp, t_field *field, t_geom *geom)
 {
-	// char		*color;
-	// t_color	out;
-	// static int x = 0;
-	// static int y = 0;
-	
-	
 	(void)vp;
 	(void)field;
 	(void)geom;
-	// color = geom->bumpmap.buff.buffer[(int)(y * geom->bumpmap.buff.line_bytes + x * geom->bumpmap.buff.pixel_bits / 8)];
+	// t_vec3	tmp;
+	int	x;
+	int	y;
 
+	x = 0;
+	y = 0;
+	if (vp.geom->type == PLANE)
+	{
+		x = fabs(vp.pt.pt.x);
+		y = fabs(vp.pt.pt.y);
+		// tmp = resta_vector(vp.geom->vp.pt , vp.pt.pt);
+		if (fabs(vp.pt.pt.x) > geom->bumpmap.width - 1)
+			y = abs((int)vp.pt.pt.x % geom->bumpmap.width);
+		if (fabs(vp.pt.pt.y) > geom->bumpmap.height - 1)
+			x = abs((int)vp.pt.pt.y % geom->bumpmap.height);
+		
+		printf("IMAGE WIDTH : %d HEAIGHT %d\n", geom->bumpmap.width, geom->bumpmap.height);
+		printf("X %d Y %d\n", x, y);
+		printf("LIGHT ADJUSTMENT %f\n", geom->bumpmap.z_map[x + y * (geom->bumpmap.width)]);
+		//printf("%d\n", x + (y * geom->bumpmap.width));
+		if (geom->bumpmap.z_map[x + (y * geom->bumpmap.width )] > 0.0)
+			vp.pt.v = prod_cte_vector(geom->bumpmap.z_map[x + (y * geom->bumpmap.width )], vp.pt.v);
+		vp.pt.pt = suma_vector(vp.pt.pt, vp.pt.v);
+	}
 	// y++;
 	// x++;
 	// if (y > geom->bumpmap.height)
@@ -47,5 +63,5 @@ t_color set_pixel_color_bumpmap(t_int_pts vp, t_field *field, t_geom *geom)
 	// // 	return (field->chckbd.c1);
 	// // else
 	// // 	return (field->chckbd.c2);
-	return (add_color("255, 255, 255, 0"));
+	return (vp.geom->color);
 }
