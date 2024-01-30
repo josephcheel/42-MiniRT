612a90e32554c00825c6463fe568ab476841f091
eavedill <eavedillo@yahoo.es>
2024-01-30 11:54:49 +0100

    missing from last commit

--- bonus/src/geometry/get_int_pt_bonus.c
+++ bonus/src/geometry/get_int_pt_bonus.c
@@ -6,7 +6,7 @@
 /*   By: eavedill <eavedill@student.42barcelona>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/03 20:17:30 by jcheel-n          #+#    #+#             */
-/*   Updated: 2024/01/30 11:10:05 by eavedill         ###   ########.fr       */
+/*   Updated: 2024/01/30 11:54:49 by eavedill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,30 +22,6 @@ static bool	is_bhd_cam(t_vec3 pint, t_vec3 pi, t_vec3 vx)
 	if (aux > 0)
 		return (false);
 	return (true);
-}
-
-static t_int_pts	*get_min_vect(t_int_pts *cur, t_vec_pos *new,
-						t_geom *geom, t_vec_pos *pixl)
-{
-	t_int_pts	*out;
-	double		long_cur;
-	int			i;
-
-	out = cur;
-	if (!new)
-		return (out);
-	long_cur = modulo_vector(resta_vector(cur->pt.pt, pixl->pt));
-	i = -1;
-	while (++i < 2)
-	{
-		if (modulo_vector(resta_vector(new[i].pt, pixl->pt)) < long_cur)
-		{
-			out->pt = new[i];
-			out->pt.c = geom->color;
-			out->geom = geom;
-		}
-	}
-	return (out);
 }
 
 t_vec_pos	*get_int_pt(t_vec_pos *vps, t_geom *geo)
@@ -67,45 +43,6 @@ t_vec_pos	*get_int_pt(t_vec_pos *vps, t_geom *geo)
 	return (out);
 }
 
-static void	create_ref(t_vec_pos *vps, t_int_pts *vp_int, t_vec_pos *out, \
-			t_geom *ptr)
-{
-	double	ax;
-
-	vp_int = get_min_vect(vp_int, out, ptr, vps);
-	vp_int->ref.vz = vp_int->pt.v;
-	ax = round(prod_escalar(vp_int->pt.v, create_vect(0, 0, 1)) * 1E5) / 1E5;
-	if (ax == 1 || ax == -1)
-	{
-		if (vp_int->ref.vz.z == 0)
-		{
-			vp_int->ref.vz = create_vect(0, 0, 1);
-			vp_int->ref.vy = create_vect(0, 1, 0);
-		}
-		if (vp_int->ref.vz.z > 0)
-			vp_int->ref.vy = create_vect(0, 1, 0);
-		else
-			vp_int->ref.vy = create_vect(0, -1, 0);
-		vp_int->ref.vx = create_vect(1, 0, 0);
-	}
-	else
-	{
-		vp_int->ref.vx = conv_v_unit(prod_vectorial(\
-							create_vect(0, 0, 1), vp_int->ref.vz));
-		vp_int->ref.vy = conv_v_unit(\
-							prod_vectorial(vp_int->ref.vz, vp_int->ref.vx));
-	}
-}
-/*
-to print in case of error.
-//	vp_int->pt.v = prod_cte_vector(ptr->sense, vp_int->pt.v);
-//	ft_print_vec3("Valor de vp.pt", vp_int->pt.pt);
-//	ft_print_vec3("Valor de vp.v", vp_int->pt.v);
-//	printf("valores de aux %f -- %f\n", ax[1], ax[0]);ax[0] == 0 ||
-//		ft_print_vec3("Valor de vp.vz ", vp_int->ref.vz);
-//		ft_print_vec3("Valor de vp.vx ", vp_int->ref.vx);
-//		ft_print_vec3("Valor de vp.vy ", vp_int->ref.vy);
-*/
 
 static void get_colored_loop(t_vec_pos *vps, t_field *field, \
 								t_int_pts *vp_int, t_geom *ptr)
@@ -146,7 +83,8 @@ void	get_colored_int_pt(int pixel, t_field *field)
 		get_colored_loop(vps, field, vp_int, ptr);
 		ptr = ptr->next;
 	}
-	if (vp_int->geom && field->chckbd.is_chckbd && vp_int->geom->is_chckbd == true)
+	if (vp_int->geom && field->chckbd.is_chckbd && \
+		vp_int->geom->is_chckbd == true)
 		vp_int->pt.c = set_pixel_color_chckdb(*vp_int, field);
 	if (field->chckbd.is_light)
 		vp_int->pt.c = set_pixel_color(*vp_int, field, *vps);
